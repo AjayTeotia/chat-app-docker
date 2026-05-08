03d64915a32d7a744acb5325a67416287c407df0
AjayTeotia <teotiaajay20@gmail.com>
2026-05-08 15:32:54 +0530

    5.message-controller-get_user-for_sidebar-getMessages-sendMessages

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,19 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
