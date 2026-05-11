ae637ccc6fc4c70dff4defaa90b7dddc7f35e14c
AjayTeotia <teotiaajay20@gmail.com>
2026-05-11 14:21:40 +0530

    15.docker

--- frontend/src/pages/login-page.jsx
+++ frontend/src/pages/login-page.jsx
@@ -104,7 +104,7 @@ export function LoginPage() {
                   Loading...
                 </>
               ) : (
-                "Create Account"
+                "Login"
               )}
             </button>
           </form>
--- frontend/src/store/use-auth-store.js
+++ frontend/src/store/use-auth-store.js
@@ -4,8 +4,7 @@ import toast from "react-hot-toast";
 
 import { axiosInstance } from "../lib/axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
